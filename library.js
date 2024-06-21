69c50a966020805f57b4ada6799f30bb3a13eb8b
vitaly <vitalijdietrich@gmail.com>
2024-06-21 02:03:57 +0200

    added a read button toggle that adds a checked image on top right of the track. Also added a delete button that deletes the current book from the DOM

--- library.js
+++ library.js
@@ -6,7 +6,11 @@ const mainContainer = document.querySelector('.main-container');
 const bookContainer = document.querySelector('.book-container');
 
 
+
+
 let myLibrary = [];
+
+/* let bookRead = false; */
 
     AddNewBookButton.addEventListener('click', () => {
 
@@ -29,14 +33,14 @@ function Book(title, author, pageNum) {
     this.title = title;
     this.author = author;
     this.pageNum = pageNum;
-    this.readMarker = function() {
-        return 'Not read yet';
-    }
+   
     this.info = function () {
         return `${title} by ${author}, ${pageNum} pages, ${this.readMarker()}`
     }
   
 }
+
+
 
 
 function addBookToLibrary(array) {
@@ -45,32 +49,73 @@ function addBookToLibrary(array) {
         
         const bookDiv = document.createElement('div');
         bookDiv.classList.add('book');
+        bookDiv.dataset.read = 'false';
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('deleteButton');
+
+        const readButton = document.createElement('button');
+        readButton.classList.add('readButton');
 
         const title = document.createElement('p');
         const author = document.createElement('p');
         const pages = document.createElement('p');
+
         const backgroundImg = document.createElement('img');
+        backgroundImg.classList.add('backroundImg');
         backgroundImg.src = "../Library-App/images/newBook.jpg"; 
-        backgroundImg.alt = "Image from Alabaster Co on Unsplash";
+        backgroundImg.alt = "Image from Henry Be on Unsplash";
   
 
         title.textContent = `Title: ${book.title}`;
         author.textContent = `Author: ${book.author}`;
         pages.textContent = `Number of Pages: ${book.pageNum}`;
+        deleteButton.textContent = 'Delete';
+        readButton.textContent = 'Read';
 
         bookDiv.appendChild(backgroundImg);
         bookDiv.appendChild(title);
         bookDiv.appendChild(author);
         bookDiv.appendChild(pages);
-        
+        bookDiv.appendChild(deleteButton);
+        bookDiv.appendChild(readButton);
 
         bookContainer.appendChild(bookDiv);
 
-       
-        })
 
+        })
     }
 
+
+  
+    bookContainer.addEventListener('click', (event) => {
+        if(event.target.classList.contains('deleteButton')) {
+            const bookDiv = event.target.parentElement;
+            bookContainer.removeChild(bookDiv);
+        }
+
+        if(event.target.classList.contains('readButton')) {
+            const bookDiv = event.target.parentElement;
+            const readBookTag = bookDiv.querySelector('.readBookImg');
+            const isRead = bookDiv.dataset.read === 'true';
+    
+            if(isRead && readBookTag) {
+                bookDiv.removeChild(readBookTag)
+                bookDiv.dataset.read = 'false';
+            } else if(!isRead && !readBookTag) {
+                const readBookImg = document.createElement('img');
+                readBookImg.classList.add('readBookImg');
+                readBookImg.src = '../Library-App/images/checked.png'
+                readBookImg.alt = 'Image by Freepic';
+                bookDiv.appendChild(readBookImg);
+                bookDiv.dataset.read = 'true';
+            }
+        }
   
 
+        
+    })
 
+    
+
+ 
